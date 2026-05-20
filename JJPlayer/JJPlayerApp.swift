09a736422672aed04cbc4a700b6dea9bb5f0ea26
Jiao <343001118@qq.com>
2026-05-20 17:08:57 +0800

    Initial Commit

---
+++ JJPlayer/JJPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JJPlayerApp.swift
+//  JJPlayer
+//
+//  Created by 我勒个去去 on 2026/5/20.
+//
+
+import SwiftUI
+
+@main
+struct JJPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JJPlayerTests/JJPlayerTests.swift
@@ -0,0 +1,19 @@
+//
+//  JJPlayerTests.swift
+//  JJPlayerTests
+//
+//  Created by 我勒个去去 on 2026/5/20.
+//
+
+import Testing
+@testable import JJPlayer
+
+struct JJPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
